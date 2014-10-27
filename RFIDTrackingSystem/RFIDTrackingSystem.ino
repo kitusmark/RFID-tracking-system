ae691511a9b1bc2c6d7f4255bf6f1ebcceaff68d
kitusmark <electronicslab@icloud.com>
2014-10-27 22:58:00 +0100

    Basic structure main file

---
+++ RFIDTrackingSystem/RFIDTrackingSystem.ino
@@ -0,0 +1,27 @@
+//RFID Tracking System
+//RFID Sensor + Arduino UNO + LCD i2c Shield + RTC Module
+//System to keep track of employees at any small, cool and Open Source company
+
+//Project for RepRapBCN Headquarters - Oct 2014
+//Marc Cobler
+//Jordi Calduch
+
+//Libraries and constant definitions
+
+
+
+//Global Scope Variables
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly: 
+  
+}
+
+//----------------------------FUNCTIONS------------------------------
+
+
